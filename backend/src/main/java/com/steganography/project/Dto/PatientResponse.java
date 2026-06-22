76558cfb219dcdc1e6dd5bc89f06e5a9290cfb53
akash <akashiever@gmail.com>
2026-06-22 17:15:01 +0530

    add new files2

--- backend/src/main/java/com/steganography/project/Dto/PatientResponse.java
+++
@@ -1,9 +0,0 @@
-package com.steganography.project.Dto;
-
-public class PatientResponse {
-
-    private Long id;
-    private String username;
-    private String fullName;
-    private String medicalId;
-}
--- frontend/src/app/pages/Doctor/doctor-dashboard/doctor-dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-doctor-dashboard',
-  templateUrl: './doctor-dashboard.component.html'
-})
-export class DoctorDashboardComponent {
-}
-
